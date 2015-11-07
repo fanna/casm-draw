22df6a42bfe9378fbd3530ddaf16446e12740a05
fanna <fanna993@gmail.com>
2015-11-07 20:26:23 +0100

    Add prototype drawing

--- main.c
+++ main.c
@@ -37,17 +37,62 @@ void create_header(HEADER h, FILE *tga)
     fputc(h.desc, tga);
 }
 
-void draw_rect(int r, int g, int b, int x, int y, FILE *tga)
+void draw(int r, int g, int b, FILE *tga)
 {
-    for(x = 0; x < 640; x++)
-      for(y = 0; y < 480; y++)
+    for(int x = 0; x < 640; x++)
+      for(int y = 0; y < 480; y++)
       {
-        fputc(b%256, tga); //B
-        fputc(g%256, tga); //G
-        fputc(r%256, tga); //R
+        if(x < 80)
+        {
+          fputc(100%256, tga); //B
+          fputc(128%256, tga); //G
+          fputc(200%256, tga); //R
+        }
+        else if(x > 80 && x <160)
+        {
+          fputc(100%256, tga); //B
+          fputc(100%256, tga); //G
+          fputc(243%256, tga); //R
+        }
+        else if( x > 160 && x < 240)
+        {
+          fputc(50%256, tga); //B
+          fputc(180%256, tga); //G
+          fputc(203%256, tga); //R
+        }
+        else if(x > 240 && x < 320)
+        {
+          fputc(150%256, tga); //B
+          fputc(250%256, tga); //G
+          fputc(203%256, tga); //R
+        }
+        else if(x > 320 && x < 400)
+        {
+          fputc(230%256, tga); //B
+          fputc(90%256, tga); //G
+          fputc(203%256, tga); //R
+        }
+        else if(x > 400 && x < 480)
+        {
+          fputc(200%256, tga); //B
+          fputc(180%256, tga); //G
+          fputc(23%256, tga); //R
+        }
+        else if(x > 480 && x < 560)
+        {
+          fputc(78%256, tga); //B
+          fputc(111%256, tga); //G
+          fputc(111%256, tga); //R
+        }
+        else
+        {
+          fputc(190%256, tga); //B
+          fputc(100%256, tga); //G
+          fputc(222%256, tga); //R
+        }
+
       }
 }
-
 
 int main ()
 {
@@ -70,7 +115,7 @@ int main ()
     tga = fopen("picture.tga", "w+");
 
     create_header(header, tga);
-    draw_rect(128, 252, 256, 0, 0, tga);
+    draw(256, 256, 256, tga);
 
     fclose(tga);
 
